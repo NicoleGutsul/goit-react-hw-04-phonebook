99778a4abcb77148081aaa7b709f91997542dce8
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-09-23 19:49:36 +0100

    imports components to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,82 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+import { nanoid } from 'nanoid'
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem('contacts') !== null) {
+      const phoneData = JSON.parse(localStorage.getItem('contacts'));
+      this.setState({ contacts: phoneData });
+    }
+  }
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  addContacts = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    const compareContact = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    compareContact
+      ? alert(`${name} is already in contacts`)
+      : this.setState(prevState => ({
+          contacts: [contact, ...prevState.contacts],
+        }));
+  };
+
+  changeFilter = e => {
+    this.setState({filter: e.currentTarget.value})
+  }
+
+  getFilteredContacts = () => {
+    const {contacts, filter} = this.state;
+
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact => 
+      contact.name.toLowerCase().includes(normalizedFilter),);
+  }
+
+
+  render () {
+    const {filter} = this.state;
+    const filteredContacts = this.getFilteredContacts();
+
+    return (
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={this.addContacts}/>
+      <h2>Contacts</h2>
+      <Filter 
+        value={filter} 
+        onChange={this.changeFilter}/>
+      <ContactList 
+        contacts={filteredContacts} 
+        onDeleteContact={this.deleteContact}/>    
+    </>
+    );
+  }
 };
